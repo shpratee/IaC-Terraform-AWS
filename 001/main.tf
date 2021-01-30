d5e3801e8295781ca06f58b3c093a6e1848e8f25
shpratee <prateek.xa.sharma@gmail.com>
2021-01-30 16:01:12 +0000

    Adding variables

--- 001/main.tf
+++ 001/main.tf
@@ -2,29 +2,40 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "example" {
-  ami             = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  ami                     = "ami-0c55b159cbfafe1f0"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public ip address of the web server"
 }
